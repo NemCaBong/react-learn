a14f007bcafa5c7e2afc02fe26c9680764ef8d24
NemCaBong <nemcabong@gmail.com>
2024-02-28 17:36:59 +0700

    conditional renderiung

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,19 +1,43 @@
 import React from "react";
 
 class DisplayInfo extends React.Component {
+  state = {
+    isShowListUser: true,
+  };
+  handleShowHide = () => {
+    this.setState({
+      isShowListUser: !this.state.isShowListUser,
+    });
+  };
   render() {
     const { listUsers } = this.props;
     return (
-      <div>
-        {listUsers.map((user) => {
-          return (
-            <div key={user.id}>
-              <p>My name is {user.name}</p>
-              <p>My age is {user.age}</p>
-            </div>
-          );
-        })}
-      </div>
+      <>
+        <div>
+          <br />
+          <br />
+          <span
+            onClick={() => {
+              this.handleShowHide();
+            }}
+          >
+            {this.state.isShowListUser ? "Hide" : "Show"} list user:
+          </span>
+        </div>
+
+        {this.state.isShowListUser && (
+          <div>
+            {listUsers.map((user) => {
+              return (
+                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                  <p>My name is {user.name}</p>
+                  <p>My age is {user.age}</p>
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </>
     );
   }
 }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -5,9 +5,9 @@ import DisplayInfo from "./DisplayInfo";
 class MyComponent extends React.Component {
   state = {
     listUsers: [
-      { id: 1, name: "Hoàng", age: 21 },
-      { id: 2, name: "Trang", age: 20 },
-      { id: 3, name: "Huy", age: 10 },
+      { id: 1, name: "Hoàng", age: "21" },
+      { id: 2, name: "Trang", age: "20" },
+      { id: 3, name: "Huy", age: "10" },
     ],
   };
   render() {
