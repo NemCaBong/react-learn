f36ec776c4944b3e6464234002572159e0c7ec8e
NemCaBong <nemcabong@gmail.com>
2024-02-28 13:55:42 +0700

    Lam viec voi component co ban

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,27 +1,18 @@
-//  class component
-
-// function component: 16.8 thì nó có tác dụng giống hệt
-// class component. Trc đó thì nó ko có tác dụng gì khi không dùng với hooks.
 import React from "react";
 
 class MyComponent extends React.Component {
+  // biến state
   state = {
     name: "Hoàng",
     age: 21,
     address: "Hà Nội",
   };
-
-  // render() là phương thức bắt buộc phải có của class component
-  // tập hợp các thẻ html rồi trả ra màn hình
-  // sử dụng cú pháp JSX để viết html
-  // giúp mình viết JS bên trong code html
-
-  // bên trong JSX có thể trả về chỉ là 1 khối mà thôi
-  // nếu trả về 2 cái div thì lỗi ngay
   render() {
     return (
       <div>
-        <h1>Hello, I'm a {this.state.name}</h1>
+        <h1>Hello, I'm {this.state.name}</h1>
+        <p>I'm {this.state.age} years old</p>
+        <p>I'm from {this.state.address}</p>
         <p>
           This is the {Math.round(Math.random() * 10)} times i have written JSX
         </p>
